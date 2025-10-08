611c0111ca9cd5b6ab4f5c3ec68f6f06d8df4cc8
Justyn Hunter <cjhunter@me.com>
2025-10-07 22:04:51 -0500

    useRef for timeout

--- lib/hooks/useDebounce.ts
+++ lib/hooks/useDebounce.ts
@@ -1,21 +1,26 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 
+/**
+* useDebounce()
+*
+* debounces the given function
+*/
 export function useDebounce<T extends (...args: Parameters<T>) => void>(
     fn: T,
     delayInMs = 400,
     callbackDeps: React.DependencyList = [],
 ) {
-    let timeout: NodeJS.Timeout | null;
+    const timeoutRef = useRef<NodeJS.Timeout>(null);
 
     const debouncer = function(this: ThisParameterType<T>, ...args: Parameters<T>) {
-        if (timeout) {
-            clearTimeout(timeout);
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
         }
 
         // eslint-disable-next-line @typescript-eslint/no-this-alias
         const context = this;
-        timeout = setTimeout(() => {
-            timeout = null;
+        timeoutRef.current = setTimeout(() => {
+            timeoutRef.current = null;
             fn.apply(context, args);
         }, delayInMs);
     };
